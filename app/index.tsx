1c8e9cd7c0d84ec1c0bc85567462986851fa4420
Annie Yu <yua6@wit.edu>
2024-11-19 13:17:26 -0500

    Set up

---
+++ app/index.tsx
@@ -0,0 +1,17 @@
+//Applications Main Screen
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
+
